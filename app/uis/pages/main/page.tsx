b056ec55708cd5ab8ba9ae5ba1a06f1a132bdb90
kajol-m <kajolmurmu29gmail.com>
2025-09-17 21:14:11 +0530

    1

--- app/uis/pages/main/page.tsx
+++ app/uis/pages/main/page.tsx
@@ -20,8 +20,12 @@ const SpotifyProfile: React.FC = () => {
 
         const data = await res.json(); // backend should send back the Spotify user details
         setUser(data);
-      } catch (err: any) {
-        setError(err.message);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("An unknown error occurred");
+        }
       }
     };
 
